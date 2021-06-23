5f2db4e87fdea80a9491c3181c86b4d209316ccb
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-06-23 18:07:30 +0500

    Path Module added

---
+++ lecture#6/PathModule.js
@@ -0,0 +1,6 @@
+const path = require("path");
+console.log(path.dirname("E:/NodeJs/lecture#6/PathModule.js"));
+console.log(path.basename("E:/NodeJs/lecture#6/PathModule.js"));
+console.log(path.extname("E:/NodeJs/lecture#6/PathModule.js"));
+console.log("Now using Parse method");
+console.log(path.parse("E:/NodeJs/lecture#6/PathModule.js"));
