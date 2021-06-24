467b74bbc351af27079f26133d01312830799c6b
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-06-24 13:49:57 +0500

    pipe method

---
+++ Streams & buffers/Pipeline.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer();
+server.on("request", (req, res) => {
+  const rstream = fs.createReadStream("data.txt");
+  //   rstream.on("data", (data) => {
+  //     res.write(data);
+  //   });
+  //   rstream.on("end", () => {
+  //     res.end();
+  //   });
+  //   rstream.on("error", (err) => {
+  //     console.log(err);
+  //     res.write("not found");
+  //   });
+  //------------------------------------2nd method----------------------
+  rstream.pipe(res);
+});
+server.listen(8000, "127.0.0.1");
