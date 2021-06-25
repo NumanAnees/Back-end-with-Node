f3ee0b1f4c2e19e4e9660ef337bb6a5d0fc1854f
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-06-25 20:25:40 +0500

    Partials added

--- {------ExpressJs------}/Partials/app.js
+++ {------ExpressJs------}/Partials/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-tempPath = path.join(__dirname, "/NewViews");
+const hbs = require("hbs");
+tempPath = path.join(__dirname, "/NewViews/Views");
+partialpath = path.join(__dirname, "/NewViews/Partials");
 //set view engine
 app.set("view engine", "hbs");
 //set views dir to anyother
 app.set("views", tempPath);
+
+//Now register partial by setting route
+hbs.registerPartials(partialpath);
 //template engine route
 app.get("", (req, res) => {
   res.render("index", { name: "Numan" });
