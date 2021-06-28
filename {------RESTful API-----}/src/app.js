39110ca0a2356db92467b3bf72cffb5525a32447
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-06-28 22:38:21 +0500

    async await instead of promises

--- {------RESTful API-----}/src/app.js
+++ {------RESTful API-----}/src/app.js
@@ -5,19 +5,28 @@ const Student = require("./models/students");
 port = process.env.PORT || 8000;
 app.use(express.json());
 //creating a new student
-app.post("/students", (req, res) => {
-  console.log(req.body);
-  const user = new Student(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+// app.post("/students", (req, res) => {
+//   console.log(req.body);
+//   const user = new Student(req.body);
+//   user
+//     .save()
+//     .then(() => {
+//       res.status(201).send(user);
+//     })
+//     .catch((e) => {
+//       res.status(400).send(e);
+//     });
+// });
+//----------------------Using async/await------------------------------------
+app.post("/students", async (req, res) => {
+  try {
+    const user = new Student(req.body);
+    const CreateUser = await user.save();
+    res.status(201).send(CreateUser);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
-
 app.listen(port, () => {
   console.log("connection established");
 });
